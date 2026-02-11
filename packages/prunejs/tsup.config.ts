d1c251c7fb36ff128521d8b3b7c035a420401949
YoungMayor <youngmayor.dev@gmail.com>
2026-02-11 17:34:29 +0100

    Update tsup.config.ts

--- packages/prunejs/tsup.config.ts
+++ packages/prunejs/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/cli.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   clean: true,
   shims: true,
   target: "node18",
